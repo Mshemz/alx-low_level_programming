62556d1ced8901cc30f133533a04785f0838a0b2
Mshemz <mshemz@gmail.com>
2023-02-19 06:20:04 -0800

    5-printf.c5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print with proper grammar, but the outcome is a piece of
- * art,, followed by a new line.
+ * main - Entry point
  *
- * Return Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
