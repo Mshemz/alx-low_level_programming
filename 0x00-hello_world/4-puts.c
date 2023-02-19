a8d9680b3e73d9a494e175dc7a2b711cb7dc64f2
Mshemz <mshemz@gmail.com>
2023-02-19 05:40:25 -0800

    4-puts.c1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
-{
+
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new line."
+ * return 0.
+ */
+
 int main(void)
-	puts("programming is like building multilingual puzzle");
+
+{
+	puts("\"programming is like building a multilingual puzzle");
 
 	return (0);
 }
